7813157a96f7d252f1cc3d77a290c7905688075d
Ziz <zizsdl@googlemail.com>
2012-03-05 15:53:54 +0100

    Added Planets. If I have time, I improve them...

--- schwerkraft.c
+++ schwerkraft.c
@@ -2,42 +2,40 @@
 #include <SDL_image.h>
 
 SDL_Surface* screen;
-SDL_Surface* texture;
-spModelPointer mesh;
+SDL_Surface* planet_texture[10];
+spModelPointer planet[10];
 Sint32 rotation = 0;
 
 void draw_function(void)
 {
-  //Reseting Z Buffer to the maximal negative value
   spResetZBuffer();
-  //Cleaning the target (in this case the screen surface) with 0 (black).
   spClearTarget(0);
-  //Reseting the ModelViewMatrix to an identity matrix.
   spIdentity();
   
-  //Binding the texture
-  spBindTexture(texture);
+  spBindTexture(planet_texture[0]);
   
-  //Going to the "position" -8.0f
   spTranslate(0,0,-8<<SP_ACCURACY);
-  //rotation aorund every axis
-  spRotateX(rotation);
-  spRotateY(rotation);
-  spRotateZ(rotation);
-  spRotateX(SP_PI/8);
-  //Drawing the mesh and no update of the edgelist 
-  spMesh3D(mesh,0);
-  //Show it!
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    Sint32 matrix[16];
+    memcpy(matrix,spGetMatrix(),64);
+    spRotateZ(rotation+i*SP_PI/5);
+    Sint32 c = abs(spCos(rotation+i*SP_PI/5));
+    spTranslate((5<<SP_ACCURACY-1)+c,0,0);
+    spRotateX(rotation);
+    spRotateY(rotation);
+    spRotateZ(rotation);
+    spMesh3D(planet[i],0);
+    memcpy(spGetMatrix(),matrix,64);
+  }
   spFlip();
 }
 
 
 int calc_function(Uint32 steps)
 {
-  //update rotation counter
   rotation+=steps*32;
-  //if "Start" (or "Menu" for Wiz or "Home" for Caanoo or "Return" at the P) is
-  //pressed: exit
   if (spGetInput()->button[SP_BUTTON_START])
     return 1;
   return 0; 
@@ -45,42 +43,38 @@ int calc_function(Uint32 steps)
 
 void resize(Uint16 w,Uint16 h)
 {
-  //Selecting the renderTarget. It could be every surface
   spSelectRenderTarget(spGetWindowSurface());
-  //Setup of the new/resized window
   spSetPerspective(50.0,(float)spGetWindowSurface()->w/(float)spGetWindowSurface()->h,0.1,100);
 }
 
 
 int main(int argc, char **argv)
 {
-  //sparrow3D Init
   spInitCore();
   
-  //Setup
   screen = spCreateWindow();
   resize(screen->w,screen->h);
   
-  //Textures loading
-  texture = spLoadSurface("./data/planet.png");
-  //Setting alpha test to 0, because the texture has pink parts
-  spSetAlphaTest(0);
-  //Mesh loading
-  mesh = spMeshLoadObj("./data/planet.obj",texture,65535);
+  int i;
+  for (i = 0; i < 10;i++)
+  {
+    char buffer[256];
+    sprintf(buffer,"./data/planet%02i.png",i+1);
+    printf("Loading %s\n",buffer);
+    planet_texture[i] = spLoadSurface(buffer);
+    planet[i] = spMeshLoadObjSize("./data/planet.obj",planet_texture[i],65535,3<<SP_ACCURACY-2);
+  }
     
-  //Light on! There is one "default" light at 0,0,0 with color white. We just use it.
+  spSetAlphaTest(0);
   spSetLight(1);
   
-  //All glory the main loop
-  //every frame the draw_function is called
-  //every frame the calc_function is called with the past time as argument
-  //at least 10 ms have to be between to frames (max 100 fps)
-  //if the window is resized, the resize feedback-function is called (again)
   spLoop(draw_function,calc_function,10,resize);
   
-  //Winter Wrap up, Winter Wrap up
-  spMeshDelete(mesh);
-  SDL_FreeSurface(texture);
+  for (i = 0; i < 10;i++)
+  {
+    spMeshDelete(planet[i]);
+    SDL_FreeSurface(planet_texture[i]);
+  }
   spQuitCore();
   return 0;
 }
